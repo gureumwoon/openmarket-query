32794a993d240e640614189962e2c2b43ec3c9e2
gureumwoon <vfrvfr3221@gmail.com>
2024-03-28 14:26:42 +0900

    react-query-devtools 적용하기

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import store from "./redux/configStore";
 import ScrollToTop from './components/ScrollToTop';
 
@@ -17,6 +18,7 @@ root.render(
         <BrowserRouter>
           <ScrollToTop>
             <App />
+            <ReactQueryDevtools initialIsOpen={true} />
           </ScrollToTop>
         </BrowserRouter>
       </Provider>
