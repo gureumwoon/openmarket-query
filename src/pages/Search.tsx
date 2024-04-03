742ad3531085a3d5cff3789017d0bf9edfcbbbbb
gureumwoon <vfrvfr3221@gmail.com>
2024-04-03 23:21:17 +0900

    searchList data를 redux-toolkit에서 useQuery를 사용해서 페칭해오는 것으로 변경

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,29 +1,25 @@
-import { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom'
 import Nav from '../components/Nav'
 import SearchGrid from '../components/SearchGrid'
 import { apis } from '../shared/api'
 import { ProductDetail } from "../components/types/product";
+import { useQuery } from '@tanstack/react-query'
 
 function Search() {
     const userType = localStorage.getItem("type")
     const isLogin = localStorage.getItem("token")
     const location = useLocation();
     const search = (location.state as { search: string }).search
-    const [searchList, setSearchList] = useState([]);
 
-    useEffect(() => {
-        apis.searchProduct(search)
-            .then((res) => {
-                const list = res.data.results
-                console.log(list)
-                const filterList = list.filter((p: ProductDetail) => p.product_name.replace(" ", "").toLocaleLowerCase().includes(search.toLocaleLowerCase().replace(" ", "")))
-                setSearchList(filterList)
-            })
-            .catch((error) => {
-                console.log("서치에러", error)
-            })
-    }, [search])
+    const { data: searchList } = useQuery({
+        queryKey: ['searchProduct'],
+        queryFn: async () => {
+            const res = await apis.searchProduct(search)
+            const list = res.data.results
+            const filterList = list.filter((p: ProductDetail) => p.product_name.replace(" ", "").toLocaleLowerCase().includes(search.toLocaleLowerCase().replace(" ", "")))
+            return filterList
+        }
+    })
 
     return (
         <div>
