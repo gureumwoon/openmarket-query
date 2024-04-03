f5b45253667137567cfb8b7ac8efb3f33bf9e2f2
gureumwoon <vfrvfr3221@gmail.com>
2024-04-03 23:19:59 +0900

    interface상속을 사용해서 중복 코드를 없애고 공통된 타입 속성의 재사용성을 높임

--- src/components/types/product.ts
+++ src/components/types/product.ts
@@ -1,16 +1,23 @@
 // product
 
-export interface Product {
+interface BaseProduct {
     product_id: number;
     product_name: string;
     seller: number;
-    store_name: string;
     image: string;
     price: number;
     shipping_method: string;
     shipping_fee: number;
     stock: number;
     products_info: string;
+}
+
+export interface Product extends BaseProduct {
+    store_name: string;
+}
+
+export interface SellerProduct extends BaseProduct {
+    seller_store: string;
 }
 
 export interface ProductDetail extends Product {
@@ -20,31 +27,28 @@ export interface ProductDetail extends Product {
 
 // cart product
 
-export interface AddCart {
-    product_id: number;
-    quantity: number;
-    check: boolean;
-}
-
-export interface ModifyCartQuantity {
-    product_id: number;
-    quantity: number;
-    is_active: boolean;
-}
-
-export interface CartDetail {
-    my_cart: number;
-    cart_item_id: number;
+interface CartProduct {
     product_id: number;
     quantity: number;
     is_active?: boolean;
 }
 
+export interface AddCart extends CartProduct {
+    check: boolean;
+}
+
+export interface ModifyCartQuantity extends CartProduct {
+    is_active: boolean;
+}
+
+export interface CartDetail extends CartProduct {
+    my_cart: number;
+    cart_item_id: number;
+}
+
 // payment product
 
-export interface PaymentItem {
-    product_id: number;
-    quantity: number;
+export interface PaymentItem extends CartProduct {
     order_kind: string;
     receiver: string;
     receiver_phone_number: string;
